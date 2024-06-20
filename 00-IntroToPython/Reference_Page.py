207c252d9818cfaa3fcfaa1240f947a6982090af
Kulgen <matthewbk6@gmail.com>
2024-06-20 13:13:19 -0400

    6/20/24 1:13PM

---
+++ 00-IntroToPython/Reference_Page.py
@@ -0,0 +1,20 @@
+#Boiler Plate Code
+#import pygame
+#import sys
+
+#pygame.init()
+
+#pygame.display.set_caption("Hello Matthew Kleiner")
+
+#screen = pygame.display.set_mode((640, 480))
+#while True:
+#    for event in pygame.event.get():
+#        print(event)
+#        if event.type == pygame.QUIT:
+#            sys.exit()
+
+#    screen.fill(pygame.Color("White"))
+#    pygame.draw.circle(screen, "Black", (50, 50), 25, 1)
+#    pygame.draw.circle(screen, pygame.Color(255, 0, 0), (screen.get_size()[0] / 2, screen.get_size()[1] / 2), 100)
+#    pygame.draw.circle(screen, "Yellow", (0, screen.get_size()[1]), 10)
+#    pygame.display.update()
